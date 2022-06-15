16604a541b21db9337d67776f302a5a9ab51e6ce
vhdeshmukh <107527649+vhdeshmukh@users.noreply.github.com>
2022-06-15 15:24:55 +0530

    by abx

--- gitDemo/src/gitDemo/DemoGit.java
+++ gitDemo/src/gitDemo/DemoGit.java
@@ -5,6 +5,10 @@ public class DemoGit {
 	{
 		System.out.println("m1----method");
 	}
+	public void m2()
+	{
+		System.out.println("m2----method");
+	}
 	public static void main(String[] args) {
 		System.out.println("main----method");
 	}
